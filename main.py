d0c886f53f46a2370dea807cb64df4a5ef6fd9ca
Tejaswi Gautam <tejaswigautam@gmail.com>
2024-04-13 20:22:30 -0700

    Added default message input on the chat interface

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with st.chat_message("assistant")
+    st.write("Hello Teja. Welcome!")
+
 prompt = st.chat_input("Say Something...")
 if prompt:
     st.write(f"User has sent the following message: {prompt}")
