10c2fc4e17933c38669b493af2ac0b5fd06dce26
Tejaswi Gautam <tejaswigautam@gmail.com>
2024-04-16 23:03:45 -0700

    Add widget

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.title("Chat With Teja")
 
+x = st.slider('x')
+st.write(x,'squared is', x*x)
+
+
 with st.chat_message("assistant"):
     st.write("Hello Teja. Welcome!")
 
