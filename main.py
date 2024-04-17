e1b63ac9b4337212960dbd8f62c02d69fd25610d
Tejaswi Gautam <tejaswigautam@gmail.com>
2024-04-16 23:13:12 -0700

    Add sidebar

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ st.title("Chat With Teja")
 x = st.slider('x')
 st.write(x,'squared is', x*x)
 
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
 
 with st.chat_message("assistant"):
     st.write("Hello Teja. Welcome!")
